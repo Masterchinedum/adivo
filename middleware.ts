4c757360f35925bf4ed6f25fb7a72d6d2d999d29
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-12-18 22:09:56 +0000

    enhance middleware to handle public routes and improve authentication flow

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,31 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)', 
+  '/sign-up(.*)', 
+  '/api/webhook(.*)', 
+  '/'
+])
+
 const isAdminRoute = createRouteMatcher(['/admindash(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
+
+  if (isPublicRoute(req)) {
+      return NextResponse.next()
+    }
+
+    const { userId, redirectToSignIn } = await auth()
+
   // Protect all routes starting with `/admin`
   if (isAdminRoute(req) && (await auth()).sessionClaims?.metadata?.role !== 'admin') {
     const url = new URL('/404', req.url)
     return NextResponse.redirect(url)
+  }
+  // For other protected routes, redirect to sign-in if not logged in
+  if (!userId) {
+    return redirectToSignIn()
   }
 })
 
